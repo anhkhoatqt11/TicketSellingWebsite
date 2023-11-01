bf4352ecef6b404d3b5e33714676a72491d9b979
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2023-11-01 18:03:31 +0700

    updated tsconfig

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -2,10 +2,30 @@
 
 import React from 'react'
 import ButtonTest from './component/ButtonTest';
+import { Button } from '@/components/ui/button';
+import DialogCustom from '@/components/ui/dialogCustom';
+
 const page = () => {
+
+    const [openPreview, setOpenPreview] = React.useState(false);
+
     return (
         <div>
-            <ButtonTest/>
+            <Button
+                className="rounded-full text-slate-50 bg-black w-[42px] h-[42px] text-[24px] p-0 hover:bg-black transition ease-in-out duration-200 hover:scale-[1.2]"
+                onClick={() => {
+                    setOpenPreview(true);
+                }}>
+
+            </Button>
+            {openPreview ? (
+                <DialogCustom className="w-full lg:w-[80%] h-[80%] lg:h-[95%] flex items-center justify-center"
+                    setIsModalOpen={setOpenPreview}
+                    isModalOpen={openPreview}
+                    warningOnClose={true}>
+                  
+                </DialogCustom>
+            ) : null}
         </div >
     )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/src/lib/utils";
+import { cn } from "@/lib/utils";
+
 
 const buttonVariants = cva(
   `inline-flex cursor-pointer items-center justify-center rounded-md text-sm
