8d3da8082959cdce7de5a90bd5799cd4d7a5772c
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2023-11-07 14:38:04 +0700

    add register, scheme changes

---
+++ src/app/(unauthen)/auth/register/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import jwt from 'jsonwebtoken';
+import Register from './Register';
+
+const page = async ({ searchParams }: { searchParams: unknown }) => {
+    let email = null;
+    let name = null;
+    jwt.verify(
+      searchParams.payload,
+      process.env.NEXT_PUBLIC_JWT_SECRET,
+      (err, decoded) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        email = decoded?.email;
+        name = decoded?.name;
+      }
+    );
+  
+    return (
+        <div className="p-12 relative h-screen w-full ">
+            <div className="lg:p-8 sm:p-12 ">
+                <div className="mx-auto h-full flex w-full flex-col justify-center space-y-6 ">
+                    <div className="flex flex-col space-y-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">
+                            Đăng ký tài khoản
+                        </h1>
+                        <p className="text-sm text-muted-foreground">
+                            Đăng ký vào trang bán vé sự kiện
+                        </p>
+                    </div>
+                    <Register payload={{ email, name }} />
+                </div>
+            </div>
+        </div>)
+}
+
+export default page
