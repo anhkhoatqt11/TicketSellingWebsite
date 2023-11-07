a011beb27d1c3b67050432e012c05cf9fe3a86b1
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2023-11-07 13:08:46 +0700

    Restruct directory

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -11,22 +11,7 @@ const page = () => {
 
     return (
         <div>
-            <Button
-                className="rounded-full text-slate-50 bg-black w-[42px] h-[42px] text-[24px] p-0 hover:bg-black transition ease-in-out duration-200 hover:scale-[1.2]"
-                onClick={() => {
-                    setOpenPreview(true);
-                }}>
-
-            </Button>
-            {openPreview ? (
-                <DialogCustom className="w-full lg:w-[80%] h-[80%] lg:h-[95%] flex items-center justify-center"
-                    setIsModalOpen={setOpenPreview}
-                    isModalOpen={openPreview}
-                    warningOnClose={true}>
-                  
-                </DialogCustom>
-            ) : null}
-        </div >
+        </div>
     )
 }
 
